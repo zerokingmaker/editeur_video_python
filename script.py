fcf139a889a0c6f5fa3c1fe81af345b135af834d
zerokingmaker <85169323+zerokingmaker@users.noreply.github.com>
2023-01-06 22:49:48 +0000

    Rajouté la fonction main() pour gérer le program
    Grâce à cela on peut quitter le programme quand on le souhaite ou le continuer après une edition

--- script.py
+++ script.py
@@ -29,49 +29,76 @@ def audio_editor(video_to_edit, name_of_the_edit, silence=False):
         audio = video.subclip(start_time, end_time).audio
         audio.write_audiofile("{}.mp3".format(name_of_the_edit))
 
+
 print("Bienvenue dans le vidéo editeur !")
 
-print("Voici vos différentes options. \n 1 - Le video cutter. \n 2 - Le gif maker. \n 3 - L'éditeur audio")
+print("Voici vos différentes options. \n 1 - Le video cutter. \n 2 - Le gif maker. \n 3 - L'éditeur audio \n 4 - Sortir du programme")
 
 choice = int(input("Quel est votre choix ? "))
 
-if choice == 1 :
-    video = input("Veillez choisir la video à couper : ")
-    start = input("Veillez choisir le début de la vidéo : ")
-    end = input("Veillez choisir le début de la vidéo : ")
-    new_name = input("Veillez choisir le nom de la vidéo après l'édition : ")
-
-    try :
-        video_cutter(video, start, end, new_name)
-    except :
-        print("Vous n'avez pas donné les bonnes informations")    
-
-elif choice == 2 :
-    video = input("Veillez choisir la video à editer : ")
-    width = int(input("Veillez choisir la longeur : "))
-    height = int(input("Veillez choisir la largeur : "))
-    new_name = input("Veillez choisir le nom de la vidéo après l'édition : ")
-
-    try :
-        gif_maker(video, width, height, new_name)
-    except :
-        print("Vous n'avez pas donné les bonnes informations") 
-
-elif choice == 3 :
-    video = input("Veillez choisir la video à editer : ")
-    new_name = input("Veillez choisir le nom de la vidéo après l'édition : ")
-    silence = input("Voulez-vous supprimer l'audio ? (Y/N) ")
-    if silence == 'Y' :
-        try :
-            audio_editor(video, new_name, True)
-        except :
-            print("Vous n'avez pas donné les bonnes informations") 
-    elif silence == 'N' :
-        try :
-            audio_editor(video, new_name)
-        except : 
-            print("Vous n'avez pas donné les bonnes informations") 
+def check_if_continue():
+    check = input("Voulez vous continuer ? (Y/N) ")
+    global choice
+    if check == 'N':
+        choice = 4
+    elif check == 'Y':
+        print("Voici vos différentes options. \n 1 - Le video cutter. \n 2 - Le gif maker. \n 3 - L'éditeur audio \n 4 - Sortir du programme")
+        choice = int(input("Quel est votre choix ? "))
     else :
-        print("Vous n'avez pas donné les bonnes informations") 
-else :
-    print("Vous n'avez pas donné les bonnes informations") 
+        print("Vous n'avez pas donner les bonnes informations")
+        choice = 4
+
+def main():
+    while not (choice == 4):
+        if choice == 1 :
+            video = input("Veillez choisir la video à couper : ")
+            start = input("Veillez choisir le début de la vidéo : ")
+            end = input("Veillez choisir la fin de la vidéo : ")
+            new_name = input("Veillez choisir le nom de la vidéo après l'édition : ")
+
+            try :
+                video_cutter(video, start, end, new_name)
+                check_if_continue()
+            except :
+                print("Vous n'avez pas donné les bonnes informations")
+                check_if_continue()    
+
+        elif choice == 2 :
+            video = input("Veillez choisir la video à editer : ")
+            width = int(input("Veillez choisir la longeur : "))
+            height = int(input("Veillez choisir la largeur : "))
+            new_name = input("Veillez choisir le nom de la vidéo après l'édition : ")
+
+            try :
+                gif_maker(video, width, height, new_name)
+                check_if_continue()
+            except :
+                print("Vous n'avez pas donné les bonnes informations") 
+                check_if_continue()
+
+        elif choice == 3 :
+            video = input("Veillez choisir la video à editer : ")
+            new_name = input("Veillez choisir le nom de la vidéo après l'édition : ")
+            silence = input("Voulez-vous supprimer l'audio ? (Y/N) ")
+            if silence == 'Y' :
+                try :
+                    audio_editor(video, new_name, True)
+                    check_if_continue()
+                except :
+                    print("Vous n'avez pas donné les bonnes informations")
+                    check_if_continue()
+            elif silence == 'N' :
+                try :
+                    audio_editor(video, new_name)
+                    check_if_continue()
+                except : 
+                    print("Vous n'avez pas donné les bonnes informations")
+                    check_if_continue() 
+            else :
+                print("Vous n'avez pas donné les bonnes informations")
+                check_if_continue() 
+        else :
+
+            print("Vous n'avez pas donné les bonnes informations")
+            check_if_continue()
+main() 
